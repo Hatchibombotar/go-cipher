746948487762427ba61c084a16620cf7bf41f8de
Hatchibombotar <72224591+Hatchibombotar@users.noreply.github.com>
2024-09-21 18:56:19 +0100

    let substitution cipher work with capital letters

--- cipher/substitution.go
+++ cipher/substitution.go
@@ -1,15 +1,27 @@
 package cipher
 
-func SubstitutionCipher(text string, substitutions map[rune]rune) string {
+import "errors"
+
+func SubstitutionCipher(text string, substitutions map[rune]rune) (string, error) {
+	capital_substitutions := make(map[rune]rune)
+	for k, v := range substitutions {
+		if !('a' <= k && k <= 'z') || !('a' <= v && v <= 'z') {
+			return "", errors.New("substitution key or value not a lowercase letter")
+		}
+		capital_substitutions[k-'a'+'A'] = v - 'a' + 'A'
+	}
 	decoded := ""
 	for _, char := range text {
 		substitution, sub_exists := substitutions[char]
+		capital_substitution, capital_sub_exists := capital_substitutions[char]
 		if sub_exists {
 			decoded += string(substitution)
+		} else if capital_sub_exists {
+			decoded += string(capital_substitution)
 		} else {
 			decoded += string(char)
 		}
 	}
 
-	return decoded
+	return decoded, nil
 }
--- cipher/substitution_test.go
+++ cipher/substitution_test.go
@@ -1,7 +1,6 @@
 package cipher_test
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/Hatchibombotar/go-cipher/cipher"
@@ -18,8 +17,30 @@ func TestSubstitutionCipher(t *testing.T) {
 	subsitution_map['t'] = 'g'
 	subsitution_map['y'] = 'h'
 
-	actual := cipher.SubstitutionCipher(encoded, subsitution_map)
-	fmt.Println(actual)
+	actual, err := cipher.SubstitutionCipher(encoded, subsitution_map)
+	if err != nil {
+		t.Fatalf("Expected: %s, Actual: %s", expected, actual)
+	}
+	if expected != actual {
+		t.Fatalf("Expected: %s, Actual: %s", expected, actual)
+	}
+}
+
+func TestSubstitutionCipherWithCaps(t *testing.T) {
+	encoded := "QWERTY"
+	expected := "ASDFGH"
+	subsitution_map := make(map[rune]rune)
+	subsitution_map['q'] = 'a'
+	subsitution_map['w'] = 's'
+	subsitution_map['e'] = 'd'
+	subsitution_map['r'] = 'f'
+	subsitution_map['t'] = 'g'
+	subsitution_map['y'] = 'h'
+
+	actual, err := cipher.SubstitutionCipher(encoded, subsitution_map)
+	if err != nil {
+		t.Fatalf("Expected: %s, Actual: %s", expected, actual)
+	}
 	if expected != actual {
 		t.Fatalf("Expected: %s, Actual: %s", expected, actual)
 	}
