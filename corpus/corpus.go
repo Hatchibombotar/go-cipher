3c46ec64a40300204fc5e5d8e5efea469376ea6e
Hatchibombotar <72224591+Hatchibombotar@users.noreply.github.com>
2024-10-07 18:32:19 +0100

    make corpus easily accessible

--- corpus/corpus.go
+++ corpus/corpus.go
@@ -1,22 +1,12 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-
-	"github.com/Hatchibombotar/go-cipher/analysis"
-	"github.com/Hatchibombotar/go-cipher/utils"
+	_ "embed"
 )
 
-func main() {
-	fmt.Println("analysing corpus...")
+//go:embed corpus.txt
+var corpus string
 
-	corpus := utils.ReadFile("./corpus.txt")
-	analysis_data := analysis.FullAnalysis(corpus)
-
-	jsondata, err := json.Marshal(analysis_data)
-	if err != nil {
-		panic(err)
-	}
-	utils.WriteFile("./corpus.json", jsondata)
+func GetRawCorpus() string {
+	return corpus
 }
